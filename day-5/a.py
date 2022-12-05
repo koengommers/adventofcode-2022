d7d4ef80b30af21830fe6b08c1fe5f611b14c14b
Koen Gommers <koen@koengommers.nl>
2022-12-05 11:26:03 +0100

    Day 5

---
+++ day-5/a.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import re
+
+with open('input') as f:
+    are_instructions = False
+    stacks = []
+    instructions = []
+    for line in f:
+        if line == '\n':
+            are_instructions = True
+        elif not are_instructions:
+            stacks.append(line)
+        elif are_instructions:
+            instructions.append(line)
+
+stack_indices = stacks.pop().replace('\n', '')
+stacks_dict = defaultdict(list)
+for index in stack_indices.split():
+    line_index = stack_indices.index(index)
+    for line in stacks[::-1]:
+        if line[line_index] != ' ':
+            stacks_dict[index].append(line[line_index])
+
+for instruction in instructions:
+    pattern = r'move (\d+) from (\d+) to (\d+)'
+    match = re.match(pattern, instruction)
+    amount, from_, to = match.group(1, 2, 3)
+    for _ in range(int(amount)):
+        stacks_dict[to].append(stacks_dict[from_].pop())
+
+print(''.join([x[-1] for x in stacks_dict.values()]))
