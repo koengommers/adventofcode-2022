f9035f4c0f09b05f205d2198aacc5251c33fa2d5
Koen Gommers <koen@koengommers.nl>
2022-12-06 10:40:04 +0100

    Day 6

---
+++ day-6/b.py
@@ -0,0 +1,10 @@
+MARKER_LENGTH = 14
+
+def find_marker(input):
+    for x in range(MARKER_LENGTH, len(input)):
+        check = input[x-MARKER_LENGTH:x]
+        if len(set(check)) == MARKER_LENGTH:
+            return x
+
+with open('input') as f:
+    print(find_marker(f.readline()))
