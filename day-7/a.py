6571531b562955301eabc15006cc9bb5ac5756ff
Koen Gommers <koen@koengommers.nl>
2022-12-07 11:52:03 +0100

    Day 7

---
+++ day-7/a.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+import os
+
+current_dir = None
+dirs = defaultdict(lambda: {'files': [], 'dirs': set()})
+
+with open('input') as f:
+    for line in f:
+        if line.startswith('$ cd '):
+            new_dir = line.strip().split().pop()
+            if new_dir == '..':
+                current_dir = dirs[current_dir]['parent']
+            else:
+                if current_dir != None:
+                    new_dir = os.path.join(current_dir, new_dir)
+                dirs[new_dir]['parent'] = current_dir
+                current_dir = new_dir
+        elif line.startswith('dir'):
+            new_dir = line.split().pop()
+            new_dir = os.path.join(current_dir, new_dir)
+            dirs[current_dir]['dirs'].add(new_dir)
+        elif not line.startswith('$'):
+            dirs[current_dir]['files'].append(tuple(line.split()))
+
+dir_sizes = {}
+while len(dir_sizes) != len(dirs):
+    can_calculate = set([dir for dir in dirs if dirs[dir]['dirs'].issubset(dir_sizes.keys())])
+    should_calculate = can_calculate.difference(dir_sizes.keys())
+    for dir in should_calculate:
+        filesize = sum([int(x[0]) for x in dirs[dir]['files']])
+        dirsize = sum(dir_sizes[x] for x in dirs[dir]['dirs'])
+        dir_sizes[dir] = filesize + dirsize
+
+print(sum([x for x in dir_sizes.values() if x <= 100000]))
